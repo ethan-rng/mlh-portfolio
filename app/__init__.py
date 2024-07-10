76af36cc863ed13182fa77d96f4f22a4bae8a009
Ethan Jian Song Rong <erong2@uwo.ca>
2024-07-10 16:20:00 -0400

    developed api and curl-test.sh

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,73 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
+from playhouse.shortcuts import model_to_dict
 from app.views import views
+from peewee import *
+import os
+import datetime
 
-
+# Initializing App
 app = Flask(__name__)
-app.register_blueprint(views)
+
+# Setup of Database
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+
+# ORM Models
+class TimelinePost(Model):
+    name = CharField( )
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+
+# Database Paths
+@app.route("/api/timeline_post", methods=['GET'])
+def getTimelinePost():
+    return { 'timeline_posts' : [ model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc()) ] }
+
+
+@app.route("/api/timeline_post", methods=['POST'])
+def postTimelinePost():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+ 
+
+
+@app.route("/api/timeline_post", methods=['DELETE'])
+def deleteTimelinePost():
+    name = request.form['name']
+    startDate = request.form['start'].strip('"')
+    endDate = request.form['end'].strip('"')
+    print(startDate, endDate)
+    # Convert start_date and end_date to datetime objects
+    start_date = datetime.datetime.strptime(startDate, '%a, %d %b %Y %H:%M:%S %Z')
+    end_date = datetime.datetime.strptime(endDate, '%a, %d %b %Y %H:%M:%S %Z')
+
+    query = TimelinePost.delete().where(
+        (TimelinePost.name == name) & 
+        (TimelinePost.created_at.between(start_date, end_date))
+    )
+
+    return jsonify({ "delete_count": query.execute() })
+
+
 
 if __name__ == "__main__":
+    app.register_blueprint(views)
     app.run(debug=True, port=5001)
---
+++ scripts/curl-test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# CHECKING IF JQ IS INSTALLED
+if !command -v jq &> /dev/null; then
+    echo "jq is not installed. Please install jq to proceed."
+    echo "Debian OS: sudo apt-get install jq"
+    echo "Mac OS: brew install jq"
+    exit 1
+fi
+
+
+
+# CONSTANTS
+url="http://127.0.0.1:5000/api/timeline_post"
+random_number=$((RANDOM))
+
+# POST BODY + API RESPONSE
+name="Ethan_$random_number"
+email="ethan_$random_number@example.com"
+content="This is a test post with a random number: $random_number"
+post_response=$(curl -s $url -X POST -d "name=$name&email=$email&content=$content" )
+
+# TESTING POST RESPONSE
+if [[ $(echo "$post_response" | jq -e --arg name "$name" '.name == $name') > /dev/null && \
+      $(echo "$post_response" | jq -e --arg email "$email" '.email == $email') > /dev/null && \
+      $(echo "$post_response" | jq -e --arg content "$content" '.content == $content') > /dev/null ]]; then
+    echo "Test 1/3 POST OPERATION SUCCESS | $post_response"
+else
+    echo "Test 1/3 POST OPERATION FAILED"
+    exit 1
+fi
+
+
+
+# GET API RESPONSE
+get_response=$(curl -s "$url")
+
+# TESTING GET RESPONSE
+if created_at=$(jq -e --arg name "$name" --arg email "$email" --arg content "$content" '
+        .timeline_posts[] | select(.name == $name and .email == $email and .content == $content) | .created_at
+        ' <<< "$get_response"); then
+
+    echo "Test 2/3 GET OPERATION SUCCESS | $post_response"
+else
+    echo "Test 2/3 GET OPERATION FAILED"
+    exit 1
+fi
+
+
+
+# DELETE BODY + API RESPONSE
+sleep 2
+delete_response=$(curl -s $url -X DELETE -d "name=$name&start=$created_at&end=$created_at" )
+
+# TESTING DELETE RESPONSE
+if echo "$delete_response" | jq -e '.delete_count == 1' > /dev/null; then
+    echo "Test 3/3 DELETE OPERATION SUCCESS | $delete_response"
+else
+    echo "Test 3/3 DELETE OPERATION FAILED"
+    exit 1
+fi
+
+
