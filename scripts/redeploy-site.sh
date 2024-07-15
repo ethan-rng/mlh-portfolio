55bb1fa04133e7cf6f721c2eb18f4f653e2bfa6b
Ethan Jian Song Rong <erong2@uwo.ca>
2024-07-15 16:13:22 -0400

    updated redeploy-site.sh with service daemons

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# ARCHIVED
 # Ending All Exisiting Tmux sessions
-tmux kill-server && echo "1. Killed All Running Servers Successfully"
+# tmux kill-server && echo "1. Killed All Running Servers Successfully"
 
 # Fetching From Github
 git fetch && git reset origin/main --hard && echo "2. Grabbed Remote Branch Successfully"
@@ -9,5 +10,9 @@ git fetch && git reset origin/main --hard && echo "2. Grabbed Remote Branch Succ
 # Starting Up Virtual Env and Installing Dependecies
 source python3-virtualenv/bin/activate && pip install -r "requirements.txt" && echo "3. Activated Virtual Env and Updated Dependencies"
 
+# Starting Up myportfolio Service
+systemctl daemon-reload && systemctl restart myportfolio && echo "3. Restarted the myportfolio Service"
+
+# ARCHIVED
 # Creating New Tmux Session and Starting Flask Server
-tmux new-session -d -s my_flask_session 'flask run --host=0.0.0.0 --port=80'
+# tmux new-session -d -s my_flask_session 'flask run --host=0.0.0.0 --port=80'
